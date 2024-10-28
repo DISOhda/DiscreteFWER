5b603e3d558a610ac4dd2966e5d13b2799e87948
DISOhda <diso.fbmn@h-da.de>
2024-10-28 13:11:57 +0100

    Initial package documentation

---
+++ R/DiscreteFWER-package.R
@@ -0,0 +1,25 @@
+#' @title
+#' FWER-Based Multiple Testing Procedures with Adaptation for Discrete Tests
+#' 
+#' @description
+#' This package implements the \[d-Bonf\], \[sd-d-Bonf\], \[d-Ind\], and
+#' simulation-based procedures for discrete tests (see References).
+#'
+#' @docType package
+#' @importFrom Rcpp evalCpp
+#' @useDynLib DiscreteFWER
+#' 
+#' @details
+#' Yet to come.
+#' 
+#' @references
+#' Döhler, S. (2010). Validation of credit default probabilities using
+#' multiple-testing procedures. *Journal of Risk Model Validation*, *4*(4),
+#' pp. 59-92. \doi{10.21314/JRMV.2010.062}
+#' 
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom lifecycle deprecated
+## usethis namespace: end
+NULL
