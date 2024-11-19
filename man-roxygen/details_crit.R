e11d5320948f29929db02c7e59c55d63c265b276
DISOhda <diso.fbmn@h-da.de>
2024-11-19 11:59:59 +0100

    Update description of parameters and return values; switch to snake case

--- man-roxygen/details_crit.R
+++ man-roxygen/details_crit.R
@@ -1,6 +1,7 @@
 #' @details
-#' Computing critical constants (`ret.crit.consts = TRUE`) requires considerably
+#' Computing critical constants (`critical_values = TRUE`) requires considerably
 #' more execution time, especially if the number of unique supports is large.
 #' We recommend that users should only have them calculated when they need them,
 #' e.g. for illustrating the rejection area in a plot or other theoretical
-#' reasons.
+#' reasons. Setting (`critical_values = FALSE`) is sufficient for obtaining
+#' rejection decisions and adjusted \eqn{p}-values.
--- man-roxygen/return.R
+++ man-roxygen/return.R
@@ -1,21 +1,22 @@
 #' @return
-#' A \code{DiscreteFDR} S3 class object whose elements are:
+#' A \code{DiscreteFWER} S3 class object whose elements are:
 #' \item{Rejected}{rejected raw \eqn{p}-values.}
 #' \item{Indices}{indices of rejected hypotheses.}
-#' \item{Num.rejected}{number of rejections.}
-#' \item{Adjusted}{adjusted \eqn{p}-values<%=ifelse(exists("BR") && BR, ""," (only for step-down direction)")%>.}
-#' \item{Critical.constants}{critical values (only exists if computations where performed with `ret.crit.consts = TRUE`).}
-#' \item{Select}{list with data related to \eqn{p}-value selection; only exists if `select.threshold < 1`.}
-#' \item{Select$Threshold}{\eqn{p}-value selection threshold (`select.threshold`).}
-#' \item{Select$Effective.Thresholds}{results of each \eqn{p}-value CDF evaluated at the selection threshold.}
+#' \item{Num_rejected}{number of rejections.}
+#' \item{Adjusted}{adjusted \eqn{p}-values.}
+#' \item{Critical_constants}{critical values (only exists if computations where performed with `critical_values = TRUE`).}
+#' \item{Data}{list with input data.}
+#' \item{Data$Method}{character string describing the performed algorithm, e.g. 'Discrete Bonferroni procedure'.}
+#' \item{Data$Raw_pvalues}{observed \eqn{p}-values.}
+#' \item{Data$pCDFlist}{list of the \eqn{p}-value supports.}
+#' \item{Data$FWER_level}{FWER level `alpha`.}
+#' \item{Data$Independence}{boolean indicating whether the \eqn{p}-values were considered as independent.}
+#' \item{Data$Stepdown}{boolean indicating whether a step-down or single-step procedure was performed.}
+#' \item{Data$Data_name}{the respective variable names of the input data.}
+#' \item{Select}{list with data related to \eqn{p}-value selection; only exists if `select_threshold < 1`.}
+#' \item{Select$Threshold}{\eqn{p}-value selection threshold (`select_threshold`).}
+#' \item{Select$Effective_Thresholds}{results of each \eqn{p}-value CDF evaluated at the selection threshold.}
 #' \item{Select$Pvalues}{selected \eqn{p}-values that are \eqn{\leq} selection threshold.}
 #' \item{Select$Indices}{indices of \eqn{p}-values \eqn{\leq} selection threshold.}
 #' \item{Select$Scaled}{scaled selected \eqn{p}-values.}
 #' \item{Select$Number}{number of selected \eqn{p}-values \eqn{\leq} selection threshold.}
-#' \item{Data}{list with input data.}
-#' \item{Data$Method}{character string describing the used algorithm, e.g. 'Discrete Benjamini-Hochberg procedure (step-up)'}
-#' \item{Data$Raw.pvalues}{observed \eqn{p}-values.}
-#' \item{Data$pCDFlist}{list of the \eqn{p}-value supports.}
-#' \item{Data$FDR.level}{FDR level `alpha`.}
-#' \item{Data$Data.name}{the respective variable names of the input data.}
-#' <%=ifelse(exists("BR") && BR, "\\item{DBR.Tuning}{value of the tuning parameter `lambda`.}","") %> 
